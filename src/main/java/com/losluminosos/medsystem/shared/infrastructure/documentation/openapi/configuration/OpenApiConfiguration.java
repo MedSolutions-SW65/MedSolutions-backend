3ea1b2efc129b53ecf2ac85276585bb2d9cb36c1
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-09-25 22:11:12 -0500

    chore: added cors configuration

--- src/main/java/com/losluminosos/medsystem/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/losluminosos/medsystem/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -21,7 +24,11 @@ public class OpenApiConfiguration {
                                 .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
                 .externalDocs(new ExternalDocumentation()
                         .description("MedSystem Wiki Documentation")
-                        .url("https://losluminosos-sw57.github.io/LandingPageMedSystem/"));
+                        .url("https://losluminosos-sw57.github.io/LandingPageMedSystem/"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Development Server"),
+                        new Server().url("https://cartunn.up.railway.app").description("production/ Server")
+                ));
 
 
         return openApi;
