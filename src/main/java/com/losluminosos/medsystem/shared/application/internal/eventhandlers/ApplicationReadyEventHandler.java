21d43d608bd07de37d5db308348ab372ecc2787b
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-11-05 18:21:05 -0500

    feat: modified ApplicationReadyEventHandler

--- src/main/java/com/losluminosos/medsystem/shared/application/internal/eventhandlers/ApplicationReadyEventHandler.java
+++ src/main/java/com/losluminosos/medsystem/shared/application/internal/eventhandlers/ApplicationReadyEventHandler.java
@@ -1,5 +1,7 @@
 package com.losluminosos.medsystem.shared.application.internal.eventhandlers;
 
+import com.losluminosos.medsystem.auth.domain.model.commands.SeedRolesCommand;
+import com.losluminosos.medsystem.auth.domain.services.RoleCommandService;
 import com.losluminosos.medsystem.shared.domain.model.commands.SeedSpecialtiesCommand;
 import com.losluminosos.medsystem.shared.domain.services.SpecialtyCommandService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -9,14 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class ApplicationReadyEventHandler {
     private final SpecialtyCommandService specialtyCommandService;
+    private final RoleCommandService roleCommandService;
 
-    public ApplicationReadyEventHandler(SpecialtyCommandService specialtyCommandService) {
+    public ApplicationReadyEventHandler(SpecialtyCommandService specialtyCommandService, RoleCommandService roleCommandService) {
         this.specialtyCommandService = specialtyCommandService;
+        this.roleCommandService = roleCommandService;
     }
 
     @EventListener
     public void on(ApplicationReadyEvent event){
         var seedSpecialtiesCommand = new SeedSpecialtiesCommand();
         specialtyCommandService.handle(seedSpecialtiesCommand);
+        var seedRolesCommand = new SeedRolesCommand();
+        roleCommandService.handle(seedRolesCommand);
     }
 }
